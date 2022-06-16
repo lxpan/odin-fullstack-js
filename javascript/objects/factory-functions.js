eed8e7e80cba38fefa3879d602c0ecb326ed6aea
Luoxi Pan <l.panpax@gmail.com>
2022-06-16 17:14:48 +1000

    Add getter function for 'health'

--- javascript/objects/factory-functions.js
+++ javascript/objects/factory-functions.js
@@ -2,6 +2,7 @@ const Player = (name, level) => {
     let health = level * 2;
     const getLevel = () => level;
     const getName  = () => name;
+    const getHealth = () => health;
     const die = () => {
       // uh oh
     };
@@ -21,10 +22,15 @@ const Player = (name, level) => {
         console.log(`${name} has damaged ${enemy.getName()}`);
       }
     };
-    return {attack, damage, getLevel, getName};
+    return {attack, damage, getLevel, getName, getHealth};
   };
   
   const jimmie = Player('jim', 10);
   const badGuy = Player('jeff', 5);
-  jimmie.attack(badGuy);
-  
+  // jimmie.attack(badGuy);
+
+  // jimmie.die() // wouldn't work (is 'private')
+  console.log(jimmie.getHealth());
+  console.log(badGuy.getHealth());
+  jimmie.health -= -1000; // not operable in its current scope
+  console.log(jimmie.health);
