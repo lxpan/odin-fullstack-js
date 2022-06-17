c9b42226f12c3e54c793899cbe0519e9c2394d8d
Luoxi Pan <l.panpax@gmail.com>
2022-06-17 21:05:40 +1000

    Refactor to use private properties

--- javascript/objects/module-pattern.js
+++ javascript/objects/module-pattern.js
@@ -2,9 +2,16 @@ let myModule = (function() {
     'use strict'; // run in ES5 strict mode
     // Your code here
     // All function and variables are scoped to this function
+    let _privateProperty = 'Hello World';
+
+    function _privateMethod() {
+        console.log(_privateProperty);
+    }
+
+
     return {
         publicMethod: function() {
-          console.log('Hello World!');
+          _privateMethod();
         }
       };
   })();
