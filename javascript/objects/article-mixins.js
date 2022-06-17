94c39560ec23773b257c44926c6909fab3d3ee23
Luoxi Pan <l.panpax@gmail.com>
2022-06-17 20:21:17 +1000

    Add examples from article

---
+++ javascript/objects/article-mixins.js
@@ -0,0 +1,36 @@
+
+import Events from 'eventemitter3';
+
+const rawMixin = function () {
+  const attrs = {};
+
+  return Object.assign(this, {
+    set (name, value) {
+      attrs[name] = value;
+
+      this.emit('change', {
+        prop: name,
+        value: value
+      });
+    },
+
+    get (name) {
+      return attrs[name];
+    }
+  }, Events.prototype);
+};
+
+const mixinModel = (target) => rawMixin.call(target);
+
+const george = { name: 'george' };
+const model = mixinModel(george);
+
+model.on('change', data => console.log(data));
+
+model.set('name', 'Sam');
+/*
+{
+  prop: 'name',
+  value: 'Sam'
+}
+*/
--- javascript/objects/inheritance-with-factories.js
+++ javascript/objects/inheritance-with-factories.js
@@ -5,13 +5,18 @@ const Person = (name) => {
   
   const Nerd = (name) => {
     // simply create a person and pull out the sayName function with destructuring assignment syntax!
-    const {sayName} = Person(name);
+    // const {sayName} = Person(name);
+    const prototype = Person(name);
     const doSomethingNerdy = () => console.log('nerd stuff');
-    return {sayName, doSomethingNerdy};
+    // return {sayName, doSomethingNerdy};
+
+    // copies all numerable own properties from prototype and doSome... into {}
+    return Object.assign({}, prototype, {doSomethingNerdy}); // {sayName} & {doSomethingNerdy}
   }
   
   const jeff = Nerd('jeff');
   
   jeff.sayName(); //my name is jeff
   jeff.doSomethingNerdy(); // nerd stuff
+  console.log(jeff);
   
