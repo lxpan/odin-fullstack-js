37ceae7a0b897b5d86dcf55fd392edb77156671b
Luoxi Pan <l.panpax@gmail.com>
2022-06-18 17:23:04 +1000

    Complete article exercise

---
+++ javascript/objects/devto-article/module.js
@@ -0,0 +1,33 @@
+const documentMock = (() => ({
+    querySelector: (selector) => ({
+      innerHTML: null,
+    }),
+  }))();
+
+const myMock = (function () {  // expression
+    return ({  // another expression?
+        querySelector: (selector) => ({  // expression
+            innerHTML: null
+        })
+    })
+})();  // immediately invoked
+  
+  const Formatter = (function(doc) {
+    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
+  
+    const makeUppercase = (text) => {
+      log("Making uppercase");
+      return text.toUpperCase();
+    };
+  
+    const writeToDOM = (selector, message) => {
+      doc.querySelector(selector).innerHTML = message;
+    }
+  
+    return {
+      makeUppercase,
+      writeToDOM,
+    }
+  })(myMock || documentMock);
+
+  Formatter.writeToDOM('#target', 'foobar');
