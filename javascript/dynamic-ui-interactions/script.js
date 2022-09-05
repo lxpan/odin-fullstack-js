24ac369bbd916df9e04c0b37cd1955c25fe90017
Luoxi Pan <l.panpax@gmail.com>
2022-09-05 16:55:30 +1000

    Expand dropdown on click

--- javascript/dynamic-ui-interactions/script.js
+++ javascript/dynamic-ui-interactions/script.js
@@ -1,1 +1,17 @@
-console.log("Script enabled!");
+const dropdownStub = document.querySelector(".menu-stub");
+const dropdownContent = document.querySelector(".menu-content");
+
+const toggleExpand = (e) => {
+    if (dropdownContent.classList.contains("hidden")) {
+        dropdownContent.classList.remove("hidden");
+        dropdownContent.classList.add("expand");
+        // console.log(dropdownContent);
+    } else {
+        dropdownContent.classList.toggle("expand");
+        dropdownContent.classList.toggle("contract");
+        // dropdownContent.classList.toggle("hidden");
+        console.log(dropdownContent);
+    }
+};
+
+dropdownStub.addEventListener("click", toggleExpand);
