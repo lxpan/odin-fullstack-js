d5e8f5972afb72af7f2a39509ac227caa3da2de3
Luoxi Pan <l.panpax@gmail.com>
2022-10-04 17:24:35 +1100

    Migrate drop down logic

--- javascript/dynamic-ui-interactions/mobile-menus/script.js
+++ javascript/dynamic-ui-interactions/mobile-menus/script.js
@@ -1,1 +1,27 @@
-console.log('Foobar!!!');
+function createDropDown(stub, content) {
+    const dropdownStub = document.querySelector(stub);
+    const dropdownContent = document.querySelector(content);
+
+    const toggleExpand = (e) => {
+        if (dropdownContent.classList.contains("hidden")) {
+            dropdownContent.classList.remove("hidden");
+            dropdownContent.classList.add("expand");
+            // console.log(dropdownContent);
+        } else if (dropdownContent.classList.contains("contract")) {
+            dropdownContent.classList.toggle("contract");
+    
+        }
+        
+        else {
+            dropdownContent.classList.toggle("expand");
+            dropdownContent.classList.toggle("contract");
+            // dropdownContent.classList.toggle("hidden");
+            console.log(dropdownContent);
+        }
+    };
+
+    dropdownStub.addEventListener("click", toggleExpand);
+}
+
+
+createDropDown(".menu-stub", ".menu-content");
