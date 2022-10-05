2dc32dfc6597cf33c0fdb499c051bc9c9521ddd9
Luoxi Pan <l.panpax@gmail.com>
2022-10-05 16:08:09 +1100

    Add function to goto previous image in queue

--- javascript/dynamic-ui-interactions/image-slider/script.js
+++ javascript/dynamic-ui-interactions/image-slider/script.js
@@ -16,7 +16,16 @@ function nextImage(e) {
 }
 
 function previousImage(e) {
-    console.log("foo");
+    const displayedImage = document.querySelector('.displayed');
+    displayedImage.classList.toggle('displayed');
+    displayedImage.classList.toggle('hidden');
+
+    // ((x-1) + k) % k
+
+    currentIdx = ((currentIdx - 1) + imagesArr.length) % imagesArr.length;
+    imagesArr[currentIdx].classList.toggle('hidden');
+    imagesArr[currentIdx].classList.toggle('displayed');
 }
 
 nextImageBtn.addEventListener('click', nextImage);
+previousImageBtn.addEventListener('click', previousImage);
