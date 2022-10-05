35521fb89b9a66e1592e8c25db38af532a6fb3d7
Luoxi Pan <l.panpax@gmail.com>
2022-10-05 16:05:10 +1100

    Add function to move to next image

--- javascript/dynamic-ui-interactions/image-slider/script.js
+++ javascript/dynamic-ui-interactions/image-slider/script.js
@@ -1,1 +1,22 @@
-console.log("Foobar");
+const images = document.querySelectorAll('.slide');
+const previousImageBtn = document.getElementById('left');
+const nextImageBtn = document.getElementById('right');
+
+const imagesArr = Array.from(images);
+let currentIdx = 1;
+
+function nextImage(e) {
+    const displayedImage = document.querySelector('.displayed');
+    displayedImage.classList.toggle('displayed');
+    displayedImage.classList.toggle('hidden');
+
+    currentIdx = (currentIdx + 1) % imagesArr.length;
+    imagesArr[currentIdx].classList.toggle('hidden');
+    imagesArr[currentIdx].classList.toggle('displayed');
+}
+
+function previousImage(e) {
+    console.log("foo");
+}
+
+nextImageBtn.addEventListener('click', nextImage);
