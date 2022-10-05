d631ccdbe9efb99cbc7b96d25fff4ec97e6e4d49
Luoxi Pan <l.panpax@gmail.com>
2022-10-05 16:37:16 +1100

    Connect progress circles with images in carousel

--- javascript/dynamic-ui-interactions/image-slider/script.js
+++ javascript/dynamic-ui-interactions/image-slider/script.js
@@ -15,13 +15,46 @@ function changeImage(e) {
 
     if (e.target.id == 'left') {
         currentIdx = ((currentIdx - 1) + imagesArr.length) % imagesArr.length;
-    } else {
+    } else if (e.target.id == 'right') {
         currentIdx = (currentIdx + 1) % imagesArr.length;    
     }
 
     imagesArr[currentIdx].classList.toggle('hidden');
     imagesArr[currentIdx].classList.toggle('displayed');
+
+    if (e.target.classList.contains('circle')) {
+        e.target.classList.toggle('selected');
+    }
+
 }
+
+function createProgressCircles() {
+    const jumpToImage = (e) => {
+        const navCircleImageIdx = e.target.id;
+        currentIdx = navCircleImageIdx;
+        changeImage(e);
+    }
+
+    // create as many circles as there are images
+    const numCircles = imagesArr.length;
+    const progressCirclesDiv = document.querySelector('.progress-circles');
+
+    for (let i = 0; i < numCircles; i++) {
+        const anchor = document.createElement('a');
+        const navCircle = document.createElement('div');
+        
+        navCircle.className = 'circle';
+        navCircle.id = i;
+        navCircle.addEventListener('click', jumpToImage);
+        
+        anchor.href = '#';
+        anchor.appendChild(navCircle);
+        
+        progressCirclesDiv.appendChild(anchor);
+    }
+}
+
+createProgressCircles();
 
 nextImageBtn.addEventListener('click', changeImage);
 previousImageBtn.addEventListener('click', changeImage);
