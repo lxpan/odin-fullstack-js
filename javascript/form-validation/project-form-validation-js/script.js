a9ece2a89dda46812a0bdd6e94bc4c40d8f777c6
Luoxi Pan <l.panpax@gmail.com>
2022-10-06 17:09:19 +1100

    Add error validation for email using JS

--- javascript/form-validation/project-form-validation-js/script.js
+++ javascript/form-validation/project-form-validation-js/script.js
@@ -1,1 +1,32 @@
-console.log('Foobar!');
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
+
+email.addEventListener('input', (e) => {
+    if (email.validity.valid) {
+        emailError.textContent = '';
+        emailError.className = 'error';
+    } else {
+        showError();
+    }
+});
+
+form.addEventListener('submit', (e) => {
+    form.preventDefault();
+});
+
+function showError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = 'You will need to enter an email address.';
+    } else if (email.validity.typeMismatch) {
+        // If the field doesn't contain an email address,
+        // display the following error message.
+        emailError.textContent = "Entered value needs to be an e-mail address.";
+    } else if (email.validity.tooShort) {
+        // If the data is too short,
+        // display the following error message.
+        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    }
+
+    emailError.className = "error active";
+}
