55195243390f8d50276708afa6483871c3732e1c
Luoxi Pan <l.panpax@gmail.com>
2022-10-12 17:37:12 +1100

    Complete mdn example

---
+++ javascript/asynch-exercises/async-and-await/mdn-async-await.js
@@ -0,0 +1,8 @@
+import fetch from 'node-fetch';
+
+async function f() {
+    let response = await fetch('http://no-such-url')
+}
+
+// f() becomes a rejected promise
+f().catch( err => console.log(err)); // TypeError: failed to fetch // (*)
