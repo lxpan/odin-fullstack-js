5749a4fa6a7c95e2763fd10cc2d12413a5e76fb9
Luoxi Pan <l.panpax@gmail.com>
2022-10-20 16:42:01 +1100

    Add recursive function all()

--- javascript/computer-science/recursion-code-quiz.js
+++ javascript/computer-science/recursion-code-quiz.js
@@ -40,5 +40,23 @@ function factorial(n) {
     return n * factorial(n - 1);
 }
 
-console.log(factorial(5));
+// console.log(factorial(5));
 
+// **************************
+// Question 4: Check all values in an array
+// Write a function called all which accepts an array and a callback and returns true if every value in the array 
+// returns true when passed as parameter to the callback function
+function all(numArray, callback, idx = 0) {
+    // if we've made it to the end, then all must be true
+    if (idx > numArray.length - 1) return true;
+    // any one false callback return is a false
+    if (callback(numArray[idx]) != true) return false;
+    // console.log(`${numArray[idx]} > 7: ${callback(numArray[idx])}`);
+    return callback(numArray[idx]) && all(numArray, callback, idx + 1);
+}
+
+// let allAreLessThanSeven = all([1,2,6,3], function(num) {
+// 	return num < 7;
+// });
+
+// console.log(allAreLessThanSeven); // false
