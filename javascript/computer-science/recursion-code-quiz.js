9be1420a0e1bf9c149336590f77a3c24fe751400
Luoxi Pan <l.panpax@gmail.com>
2022-10-20 16:21:13 +1100

    Write recursive algorithm power()

--- javascript/computer-science/recursion-code-quiz.js
+++ javascript/computer-science/recursion-code-quiz.js
@@ -19,3 +19,13 @@ function sumRange(n) {
 // **************************
 // Question 2: Power function
 // Write a function called power which takes in a base and an exponent. If the exponent is 0, return 1.
+function power(base, exponent) {
+    if (exponent == 0) return 1;
+
+    return base * power(base, exponent - 1);
+}
+console.log(power(2, 4));
+// power(2, 3) = 2 * power(2,2)
+//   power(2,2) = 2 * power(2,1)
+//     power(2,1) = 2 * power(2,0)
+//       power(2,0 = 0
